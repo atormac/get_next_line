d4857bbde7519a30506a48d256bb22bda5113f4f
Aarni Torma <atorma@student.hive.fi>
2024-04-24 19:18:38 +0300

    bonus norm fix

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/21 13:42:12 by atorma            #+#    #+#             */
-/*   Updated: 2024/04/24 17:55:02 by atorma           ###   ########.fr       */
+/*   Created: 2024/04/24 19:17:31 by atorma            #+#    #+#             */
+/*   Updated: 2024/04/24 19:17:39 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 char	*get_next_line(int fd);
 
