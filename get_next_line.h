a97c2830b67f727f50f37298b6114e21344dd57e
Aarni Torma <atorma@student.hive.fi>
2024-04-24 19:32:41 +0300

    define BUFFER_SIZE if not defined

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 13:42:12 by atorma            #+#    #+#             */
-/*   Updated: 2024/04/24 17:55:02 by atorma           ###   ########.fr       */
+/*   Updated: 2024/04/24 19:31:53 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
 char	*get_next_line(int fd);
 
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,16 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 19:17:31 by atorma            #+#    #+#             */
-/*   Updated: 2024/04/24 19:17:39 by atorma           ###   ########.fr       */
+/*   Updated: 2024/04/24 19:32:20 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
 char	*get_next_line(int fd);
 
