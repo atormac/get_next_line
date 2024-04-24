6ed6f7dca4b4981b2c194b8646e671a6cd11166f
Aarni Torma <atorma@student.hive.fi>
2024-04-24 19:08:53 +0300

    Bonus complete

---
+++ get_next_line_bonus.c
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 13:42:02 by atorma            #+#    #+#             */
+/*   Updated: 2024/04/24 19:08:30 by atorma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+
+char	*pos_newline(char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(char *str);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+static char	*line_join(char *s1, size_t s1_len, char *s2, size_t s2_len)
+{
+	char	*ret;
+
+	ret = malloc(s1_len + s2_len + 1);
+	if (!ret)
+	{
+		free(s1);
+		return (NULL);
+	}
+	ft_memcpy(ret, s1, s1_len);
+	ft_memcpy(ret + s1_len, s2, s2_len);
+	ret[s1_len + s2_len] = 0;
+	free(s1);
+	return (ret);
+}
+
+static char	*read_line(int fd, char *buf)
+{
+	char	tmp_buf[BUFFER_SIZE + 1];
+	ssize_t	size_read;
+	ssize_t	total_read;
+
+	total_read = ft_strlen(buf);
+	while (1)
+	{
+		size_read = read(fd, tmp_buf, BUFFER_SIZE);
+		if (size_read <= 0)
+			break ;
+		tmp_buf[size_read] = 0;
+		buf = line_join(buf, total_read, tmp_buf, size_read);
+		total_read += size_read;
+		if (!buf || pos_newline(tmp_buf))
+			break ;
+	}
+	return (buf);
+}
+
+static char	*dup_line(char *buffer)
+{
+	char	*line;
+	char	*line_break;
+	size_t	line_length;
+	size_t	buf_len;
+
+	if (!buffer)
+		return (NULL);
+	buf_len = ft_strlen(buffer);
+	if (buf_len == 0)
+		return (NULL);
+	line_break = pos_newline(buffer);
+	line_length = line_break - buffer;
+	if (!line_break)
+		line_length = buf_len - 1;
+	line = malloc(line_length + 2);
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, buffer, line_length + 2);
+	ft_strlcpy(buffer, buffer + line_length + 1, buf_len - line_length + 2);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buf[OPEN_MAX];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= OPEN_MAX)
+		return (NULL);
+	if (buf[fd] == NULL)
+	{
+		buf[fd] = malloc(BUFFER_SIZE + 1);
+		if (!buf[fd])
+			return (NULL);
+		buf[fd][0] = 0;
+	}
+	buf[fd] = read_line(fd, buf[fd]);
+	line = dup_line(buf[fd]);
+	if (!pos_newline(line))
+	{
+		free(buf[fd]);
+		buf[fd] = NULL;
+	}
+	return (line);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/21 14:18:11 by atorma            #+#    #+#             */
+/*   Updated: 2024/04/24 19:00:45 by atorma           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <stddef.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	register unsigned char	*p1;
+	register unsigned char	*p2;
+
+	if (!dst && !src)
+		return (0);
+	p1 = (unsigned char *)dst;
+	p2 = (unsigned char *)src;
+	while (n--)
+		*p1++ = *p2++;
+	return (dst);
+}
+
+size_t	ft_strlen(char *str)
+{
+	char	*s;
+
+	s = str;
+	while (*s)
+		s++;
+	return (s - str);
+}
+
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize)
+{
+	size_t	n;
+	size_t	src_len;
+
+	n = dstsize;
+	src_len = ft_strlen(src);
+	while (n > 1 && *src)
+	{
+		*dst++ = *src++;
+		n--;
+	}
+	if (dstsize != 0)
+		*dst = 0;
+	return (src_len);
+}
+
+char	*pos_newline(char *str)
+{
+	if (!str)
+		return (NULL);
+	while (*str)
+	{
+		if (*str == '\n')
+			return (str);
+		str++;
+	}
+	return (NULL);
+}
